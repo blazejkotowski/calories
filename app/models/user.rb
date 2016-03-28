1213b9b6b373102874e0ec29ab8b3c432538925d
Błażej Kotowski <kotowski.blazej@gmail.com>
2016-03-28 18:43:28 +0200

    more validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ActiveRecord::Base
   validates_presence_of :email, :name
   validates_uniqueness_of :email
   validates :expected_calories, numericality: { greater_than_or_equal_to: 0 }
-  validates_format_of :name, with: /\A[^0-9`!@#\$%\^&*+_=]+\z/ 
+  validates_format_of :name, with: /\A[^0-9`!@#\$%\^&*+_=]+\z/
+  validates :password, length: { minimum: 6 }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,11 +24,11 @@ describe User do
   end
 
   it "is invalid with negative expected calories" do
-    sybhect.expected_calories = -10
+    subject.expected_calories = -10
     expect(subject).not_to be_valid
   end
 
-  it "validates the name" do
+  it "validates the format of name" do
     subject.name = "!Wrong Name"
     expect(subject).not_to be_valid
   end
@@ -39,7 +39,22 @@ describe User do
     expect(subject.errors.messages.try(:[], :email)).not_to be_empty
   end
 
-  it "authenticates with correct password" do
+  it "validates the length of password" do
+    subject.password = subject.password_confirmation = 'short'
+    expect(subject).not_to be_valid
   end
-  it "doesn't authenticate with wrong password"
+
+  it "authenticates with correct password" do
+    password = subject.password
+    subject.save
+
+    expect(subject.authenticate(password)).not_to be false
+  end
+
+  it "doesn't authenticate with wrong password" do
+    wrong_password = subject.password*2
+    subject.save
+
+    expect(subject.authenticate(wrong_password)).to be false
+  end
 end
