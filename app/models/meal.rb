ff36655cb61e95df42278026059cf6ff9313571d
Błażej Kotowski <kotowski.blazej@gmail.com>
2016-03-28 19:25:29 +0200

    meal validations

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,4 +1,7 @@
 class Meal < ActiveRecord::Base
-
+  validates_presence_of :consumption_date, :consumption_time, :calories_number, 
+                        :description, :user_id
+  validates :calories_number, numericality: { greater_than_or_equal_to: 0 }
+  
   belongs_to :user
 end
